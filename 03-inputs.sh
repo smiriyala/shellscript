ed460441224c823d419fdeb244eb9d1ea93b6dbd
subbum <subbu@gmail.com>
2023-04-20 04:23:51 +0100

    variables and inputs added

---
+++ 03-inputs.sh
@@ -0,0 +1,11 @@
+#0-n $* /$@, $#
+echo $1 #first argument
+echo $2 # second argument
+echo $0 # Script Name
+
+echo $*
+echo $@ # All Arguments
+
+echo $# #Number of Arguments.
+
+#first attribute to function inputs are considered as $1 and second on is $2 till $n
