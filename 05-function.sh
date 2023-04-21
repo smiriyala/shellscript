a2755c282fcf00376349d65e52a4c58de4d8e8d1
subbum <subbu@gmail.com>
2023-04-21 10:43:30 +0100

    functions added

---
+++ 05-function.sh
@@ -0,0 +1,19 @@
+
+#declare fucntions
+xyz(){
+    echo hello world from fucntion
+    echo file name  - $0
+    echo first argument - $1
+    echo second argument - $2
+    echo all arguments - $*
+    echo number of args - $#
+    echo value of a = ${a}
+    b=20
+}
+
+
+#main programme
+#calling fucntions
+a=100
+xyz 123 234
+echo value of B = ${b}
